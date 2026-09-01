a75c747c9a04591add1fbdde4a53f187d4993eed
CyberHastati <1615607411@qq.com>
2026-09-01 19:25:16 +0800

    feat: part 3 Programming a server with NodeJS and Express b: prepared for online deployment

--- part3/course/backend/index.js
+++ part3/course/backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static('dist'))
+
 let notes = [
   {
     id: '1',
@@ -88,7 +90,7 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
